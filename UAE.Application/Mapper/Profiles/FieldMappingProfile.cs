51b0ea11db894f61f10d6e5f9adcfbd5ca851520
Senko <vlad.senko2012@gmail.com>
2023-01-07 10:44:29 +0100

    change possible value for fields to string

--- UAE.Application/Mapper/Profiles/FieldMappingProfile.cs
+++ UAE.Application/Mapper/Profiles/FieldMappingProfile.cs
@@ -1,8 +1,5 @@
-using System.Text.Json;
-using MongoDB.Bson;
 using UAE.Application.Models.Category;
 using UAE.Core.Entities;
-using UAE.Shared.Extensions;
 
 namespace UAE.Application.Mapper.Profiles;
 
@@ -11,7 +8,5 @@ public static class FieldMappingProfile
     public static FieldModel ToBusinessModel(this Field field) =>
         new(field.Name,
             (int)field.Type,
-            field.PossibleValues != null
-                ? JsonSerializer.Deserialize<object[]>(field.PossibleValues?.ToJson()).Select(x => x.ToString()).ToArray()
-                : Array.Empty<string>());
+            field.PossibleValues ?? Array.Empty<string>());
 }
